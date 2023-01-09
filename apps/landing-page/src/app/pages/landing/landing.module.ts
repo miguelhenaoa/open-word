86cc9c583129a166aa3167456f1eb5e41b74120e
miguelhenao <miguel.henao@utp.edu.co>
2023-01-08 22:36:40 -0500

    import navbar component in main page

--- apps/landing-page/src/app/pages/landing/landing.module.ts
+++ apps/landing-page/src/app/pages/landing/landing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingComponent } from './landing.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 const routes: Routes = [{ path: '', component: LandingComponent }];
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, NavbarComponent, RouterModule.forChild(routes)],
 })
 export class LandingModule {}
