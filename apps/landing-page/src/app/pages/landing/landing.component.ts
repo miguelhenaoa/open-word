a08e6c372edd58c8b9f9b49e3992cba21ab06904
miguelhenao <miguel.henao@utp.edu.co>
2023-01-09 20:54:19 -0500

    add payments component

--- apps/landing-page/src/app/pages/landing/landing.component.ts
+++ apps/landing-page/src/app/pages/landing/landing.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+  paymentList = [
+    { img: 'assets/images/logo-dlocal.jpg', alt: 'dLocal' },
+    { img: 'assets/images/paymentez.jpg', alt: 'Paymentez' },
+    { img: 'assets/images/paypal.png', alt: 'PayPal' },
+    { img: 'assets/images/totalnet.jpg', alt: 'TotalNet' },
+  ];
+}
