8d7f7f65ea1832f17b788d71a6e73ef0b82d5d3e
miguelhenao <miguel.henao@utp.edu.co>
2023-02-03 14:51:44 -0500

    fix: make some changes into footer component

--- apps/landing-page/src/app/components/footer/footer.component.ts
+++ apps/landing-page/src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ow-footer',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatDividerModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
