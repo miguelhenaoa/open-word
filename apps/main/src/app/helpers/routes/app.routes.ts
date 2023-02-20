e0d271952f779d86f44c35bdfceb39dd99117c73
miguelhenao <miguel.henao@utp.edu.co>
2023-02-19 19:21:57 -0500

    feat: create Admin module and component

--- apps/main/src/app/helpers/routes/app.routes.ts
+++ apps/main/src/app/helpers/routes/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'admin' },
   {
     path: 'login',
     loadChildren: () =>
       import('../../screens/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('../../screens/admin/admin.module').then((m) => m.AdminModule),
   },
 ];
