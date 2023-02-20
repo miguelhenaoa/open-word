424083cd7f611cb433b13d973a53e85c865a3c9f
miguelhenao <miguel.henao@utp.edu.co>
2023-02-19 19:30:38 -0500

    feat: add Users module

--- apps/main/src/app/screens/admin/admin.module.ts
+++ apps/main/src/app/screens/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AdminComponent } from './admin.component';
-import { RouterModule } from '@angular/router';
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([{ path: '', component: AdminComponent }]),
-  ],
+  imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
